10350fefc072f665f13de058a2c57255f715d71f
dj-quissman <stancoderwp@gmail.com>
2021-02-05 19:03:57 +0000

    attempt reset Quiz feature

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,12 +16,16 @@ this.setState((state) => {
 })
 }
 
+handleResetClick() {
+ this,this.setState({quiz_position : 1})
+}
+
   render() {
 const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
 
     return (
       <div>
-          { isQuizEnd ? <QuizEnd /> : <QuizQuestion
+          { isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}  /> : <QuizQuestion
           quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
           showNextQuestionHandler={this.showNextQuestion.bind(this)}
         />
