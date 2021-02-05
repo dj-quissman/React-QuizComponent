ade563cbd3d36e461e5850ef72485d7dbf7111d0
dj-quissman <stancoderwp@gmail.com>
2021-02-05 16:22:13 +0000

    fix first question

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,15 +4,17 @@ import QuizQuestion from './QuizQuestion'
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
-// constructor(props) {
-//     super(props)
-//     this.state = { quiz_position: 1 }
-// }
+constructor(props) {
+    super(props)
+    this.state = { quiz_position: 1 }
+}
 
 render() {
     return  (
+        <div>
         <QuizQuestion
-        quiz_quesition = {quizData.quiz_questions[0]} />
+        quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]} />
+        </div>
     )
 }
 }
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
 
 class QuizQuestion extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { quiz_position: 1 }
-    }
+    // constructor(props) {
+    //     super(props)
+    //     this.state = { quiz_position: 1 }
+    // }
   render() {
     return (
       <div>
         <main>
           <section>
-            <p>{this.props.quiz_position}</p>
+            <p>{this.props.quiz_question.instruction_text}</p>
           </section>
           <section className="buttons">
             <ul>
-                <li>{this.state.quiz_position}</li>
+                <li>{this.props.quiz_question.answer_options[0]}</li>
             </ul>
           </section>
         </main>
